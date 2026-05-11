23ff99db0ce7f47087966f83f6258b9d2223a367
Sarthak Shrestha <143961300+SarthakStha@users.noreply.github.com>
2026-05-11 16:03:11 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string stringify(int arr []){
+        string ans = "";
+        for (int i {0}; i < 26; i++)
+            ans.push_back(arr[i]);
+            ans.push_back(',');
+        return ans;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+
+        for(string& word : strs){
+            int counter [26] {0};
+            for (const char& c : word){
+                counter[c - 'a']++;
+            }
+            groups[stringify(counter)].push_back(word);
+        }
+
+        vector<vector<string>> ans;
+        for(auto& pair : groups){
+            ans.push_back(pair.second);
+        }
+        
+        return ans;
+    }
+};
